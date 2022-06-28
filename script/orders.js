aef2a320a0a2da5eb053d96cd67bd665a2bceb79
SathMenthu <4akon4474@gmail.com>
2022-06-28 15:42:45 +0200

    init

---
+++ script/orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+  {
+    productName: "GBS",
+    productNumber: "833",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Skis",
+    productNumber: "31",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "chess",
+    productNumber: "31",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Iphone-X",
+    productNumber: "821",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "RTX 3090TI",
+    productNumber: "511",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+];
